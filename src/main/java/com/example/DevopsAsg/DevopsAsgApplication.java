730b982c72e9ea195905a9cde2f3729cd22048e3
Paresh <2022sp93036@wilp.bits-pilani.ac.in>
2023-05-25 15:35:13 +0530

    First Commit

---
+++ src/main/java/com/example/DevopsAsg/DevopsAsgApplication.java
@@ -0,0 +1,13 @@
+package com.example.DevopsAsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsAsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsAsgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DevopsAsg/DevopsAsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DevopsAsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
